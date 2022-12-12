7a7dc4bbc9c09493c8c8217081c575cc173e4af8
Princeaddo1199 <princeaddo1199@gmail.com>
2022-12-12 08:34:45 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,26 +7,11 @@
  */
 int main(void)
 {
-	int a;
-	int p;
-
-	for (a = 5; a <=9; a++)
-	{
-		for (p = 5; p <= 9; p++)
-		{
-			putchar((a % 5) + '0');
-			putchar((p % 5) + '0');
-			if (a ! = 9 || p ! = 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+	char a;
 
 
-		}
-
-
-	}
+	for (a = 'a'; a <= 'p'; a++)
+		putchar(a);
 	putchar('\n');
 	return (0);
 }
