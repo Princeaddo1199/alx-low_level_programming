cc5f3ee5c1ddf3b97e5b969bad031ec4c9456144
Princeaddo1199 <princeaddo1199@gmail.com>
2022-12-19 21:54:18 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* swap_int - swap values of two numbers
+* @b: first pointer
+* @a: second pointer
+* Retuen: void
+*/
+void swap_int(int *a, int *b)
+{
+int constant = *a;
+*a = *b;
+*b = constant;
+}
