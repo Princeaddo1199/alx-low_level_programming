7488b28265a949f41920b82e2061b3dc4ede3344
Princeaddo1199 <princeaddo1199@gmail.com>
2022-12-19 21:59:40 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* rev_string - reversing a string
+* @s: parameter s
+*/
+void rev_string(char *s)
+{
+char tmp;
+int i, lenght1, lenght2;
+
+lenght1 = 0;
+lenght2 = 0;
+
+while (s[lenght1] != '\0')
+lenght1++;
+
+lenght2 = lenght1 - 1;
+for (i = 0; i < lenght1 / 2; i++)
+{
+tmp = s[i];
+s[i] = s[lenght2];
+s[lenght2] = tmp;
+lenght2 -= 1;
+}
+}
